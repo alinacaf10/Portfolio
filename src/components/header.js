f50058d6a632d530abb9f250870241f658700dd5
alinacaf10 <100665352+alinacaf10@users.noreply.github.com>
2022-12-07 13:35:28 +0400

    Header

---
+++ src/components/header.js
@@ -0,0 +1,15 @@
+import "./header.css"
+export default function Header(){
+return(
+    <header className="main">
+        <h1>Ali Nacafov</h1>
+        <div className="comps">
+        <h4>Home</h4>
+        <h4>About</h4>
+        <h4>Resume</h4>
+        <h4>Contact</h4>
+        </div>
+    </header>
+)
+
+}

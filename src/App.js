37fc65c46968a627538e5a85dc630c115d7c334e
alinacaf10 <100665352+alinacaf10@users.noreply.github.com>
2022-12-08 16:41:30 +0400

    add main

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/header';
+import Main from './components/main';
 
 function App() {
   return (
     <div className="App">
  <Header/>
+ <Main/>
     </div>
   );
 }

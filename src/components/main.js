495c9b7492393ed9a9393078936e0909f48378ce
alinacaf10 <100665352+alinacaf10@users.noreply.github.com>
2022-12-08 16:40:44 +0400

    Add files via upload

---
+++ src/components/main.js
@@ -0,0 +1,9 @@
+import "./main.css"
+export default function Main(){
+    return(
+        <div className="mainer">
+<h1>I,m <span>Ali</span> Nacafov</h1>
+<h2>Front-End Developer</h2>
+        </div>
+)
+}

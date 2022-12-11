9c0dec560bd319fcbc7dc5460de4f1b413e0e9ff
alinacaf10 <100665352+alinacaf10@users.noreply.github.com>
2022-12-11 23:41:33 +0400

    Delete src/components directory

--- src/components/main.js
+++
@@ -1,9 +0,0 @@
-import "./main.css"
-export default function Main(){
-    return(
-        <div className="mainer">
-<h1>I,m <span>Ali</span> Nacafov</h1>
-<h2>Front-End Developer</h2>
-        </div>
-)
-}

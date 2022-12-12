8a67c0872f16d07b468eb0937ecbeed2b9d361ca
alinacaf10 <100665352+alinacaf10@users.noreply.github.com>
2022-12-12 13:11:36 +0400

    add footer

--- src/src/comps/Navbar.js
+++ src/src/comps/Navbar.js
@@ -1,14 +1,20 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { Link,useLocation } from 'react-router-dom'
 import { Reorder } from '@mui/icons-material'
 import "./Navbar.css"
 function Navbar() {
+  const [expandNavbar, setExpandNavbar]=useState(false)
+  const location=useLocation()
+
+  useEffect(()=>{
+setExpandNavbar(false)
+  },[location])
   return (
-    <div className='navbar'>
+    
+    <div className='navbar' id={expandNavbar ? "open":"close"}>
       <div className='toggleButton'>
-        <button>
-       {" "}
-       <Reorder/>{" "}
+        <button onClick={()=>{setExpandNavbar((prev)=> !prev)}}>
+       <Reorder/>
         </button>
       </div>
       <div className='links'>
